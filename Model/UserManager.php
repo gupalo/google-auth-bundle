6eac38290870e5c20ce871d3610542d96fc24988
vl <vl@playtini.ua>
2020-04-29 19:29:06 +0300

    AbstractUser

--- Model/UserManager.php
+++ Model/UserManager.php
@@ -4,6 +4,7 @@ namespace Gupalo\GoogleAuthBundle\Model;
 
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Persistence\ObjectRepository;
+use Gupalo\GoogleAuthBundle\Entity\AbstractUser;
 use Gupalo\GoogleAuthBundle\Entity\User;
 use Gupalo\GoogleAuthBundle\Repository\UserRepository;
 
@@ -30,13 +31,13 @@ class UserManager
         return $this->class;
     }
 
-    public function saveUser(User $user): void
+    public function saveUser(AbstractUser $user): void
     {
         $this->objectManager->persist($user);
         $this->objectManager->flush();
     }
 
-    public function createUser(): User
+    public function createUser(): AbstractUser
     {
         $class = $this->getClass();
 
@@ -48,22 +49,22 @@ class UserManager
         return $this->repository->count(['enabled' => true]);
     }
 
-    public function findOneByGoogleId(string $googleId): ?User
+    public function findOneByGoogleId(string $googleId): ?AbstractUser
     {
         return $this->repository->findOneByGoogleId($googleId);
     }
 
-    public function findOneByEmail(string $email): ?User
+    public function findOneByEmail(string $email): ?AbstractUser
     {
         return $this->repository->findOneByEmail($email);
     }
 
-    public function findOneByUsername(string $username): ?User
+    public function findOneByUsername(string $username): ?AbstractUser
     {
         return $this->repository->findOneByUsername($username);
     }
 
-    public function findOneByApiKey(string $email): ?User
+    public function findOneByApiKey(string $email): ?AbstractUser
     {
         return $this->repository->findOneByApiKey($email);
     }
--- Repository/UserRepository.php
+++ Repository/UserRepository.php
@@ -2,15 +2,16 @@
 
 namespace Gupalo\GoogleAuthBundle\Repository;
 
+use Gupalo\GoogleAuthBundle\Entity\AbstractUser;
 use Gupalo\GoogleAuthBundle\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @method User|null find($id, $lockMode = null, $lockVersion = null)
- * @method User|null findOneBy(array $criteria, array $orderBy = null)
- * @method User[]    findAll()
- * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method AbstractUser|null find($id, $lockMode = null, $lockVersion = null)
+ * @method AbstractUser|null findOneBy(array $criteria, array $orderBy = null)
+ * @method AbstractUser[]    findAll()
+ * @method AbstractUser[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class UserRepository extends ServiceEntityRepository
 {
@@ -19,22 +20,22 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, $entityClass);
     }
 
-    public function findOneByGoogleId(string $googleId): ?User
+    public function findOneByGoogleId(string $googleId): ?AbstractUser
     {
         return $this->findOneBy(['googleId' => $googleId]);
     }
 
-    public function findOneByEmail(string $email): ?User
+    public function findOneByEmail(string $email): ?AbstractUser
     {
         return $this->findOneBy(['email' => $email]);
     }
 
-    public function findOneByUsername(string $username): ?User
+    public function findOneByUsername(string $username): ?AbstractUser
     {
         return $this->findOneBy(['username' => $username]);
     }
 
-    public function findOneByApiKey(string $apiKey): ?User
+    public function findOneByApiKey(string $apiKey): ?AbstractUser
     {
         return $this->findOneBy(['apiKey' => $apiKey]);
     }
