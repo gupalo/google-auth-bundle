87cf6005e2a4da660e134970a3abbb8f9e06855e
akrasnogolovets <a.krasnogolovets@playtini.ua>
2021-01-08 17:59:06 +0200

    More strict supports() method to prevent matching every request

--- Security/GoogleAuthenticator.php
+++ Security/GoogleAuthenticator.php
@@ -290,11 +290,9 @@ class GoogleAuthenticator extends SocialAuthenticator
     public function supports(Request $request): bool
     {
         // continue ONLY if the current ROUTE matches the check ROUTE or has api key
-        return (
-            $this->getDevRoles() ||
-            $request->attributes->get('_route') === 'google_auth_connect_google_check' ||
-            $this->getApiKeyFromRequest($request)
-        );
+        return
+            $request->attributes->get('_route') === 'google_auth_connect_google_check'
+            || $this->getApiKeyFromRequest($request);
     }
 
     public function supportsRememberMe(): bool
